a638b80e0df210ebe5e08d5812f77ce21fdd3823
Salar Ali Mumtaz <salar@stackadapt.com>
2021-03-31 14:33:44 -0400

    Initial commit
    - Add a lcov parser
    - Add a github action

---
+++ src/index.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+const core = require("@actions/core");
+
+const lcov = require("./lcov");
+const { checkMinimumRatio } = require("./features/minimumRatio");
+// const { sendSummaryComment } = require("./features/sendSummaryComment");
+
+async function main() {
+  const compareFile = core.getInput("lcov-file");
+  const baseFile = core.getInput("base-lcov-file");
+  core.info(`lcov-file: ${compareFile}`);
+  core.info(`base-lcov-file: ${baseFile}`);
+
+  const compareFileRaw = fs.readFileSync(compareFile, "utf8");
+  if (!compareFileRaw) {
+    core.info(`No coverage report found at '${compareFile}', exiting...`);
+    return;
+  }
+
+  const baseFileRaw = fs.readFileSync(baseFile, "utf8");
+  if (!baseFileRaw) {
+    core.info(`No coverage report found at '${baseFileRaw}', exiting...`);
+    return;
+  }
+
+  const baseFileData = await lcov.parse(baseFileRaw);
+  const compareFileData = await lcov.parse(compareFileRaw);
+
+  const comparePercentage = lcov.percentage(compareFileData);
+  core.info(`Compare branch code coverage: ${comparePercentage}%`);
+
+  const basePercentage = lcov.percentage(baseFileData);
+  core.info(`Base branch code coverage: ${basePercentage}%`);
+
+  const diff = (comparePercentage - basePercentage).toFixed(2);
+  core.info(`Code coverage diff: ${diff}%`);
+
+  // await sendSummaryComment(diff, comparePercentage, compareFileData);
+  checkMinimumRatio(diff);
+
+  core.setOutput("percentage", comparePercentage);
+  core.setOutput("diff", diff);
+}
+
+try {
+  main();
+} catch (err) {
+  core.info(err);
+  core.setFailed(err.message);
+}
